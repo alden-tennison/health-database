8fcd5fcf50988a1551b9fe121fa61c5c9c513df0
David Ward <david.a.ward@duke.edu>
2020-10-12 13:37:58 -0400

    Add logging, init_db, generic validation testing, blood_type checking, add_test post route

--- client.py
+++ client.py
@@ -1,6 +1,55 @@
 import requests
 
-new_patient = {"name": "Everett", "id": 102, "blood_type": "A-"}
+print("Add new patient with good inputs")
+new_patient = {"name": "Everett", "id": 102, "blood_type": "AB-"}
 r = requests.post("http://127.0.0.1:5000/new_patient", json=new_patient)
+print(r.status_code)
+print(r.text)
+
+print("Try to add patient with missing key")
+new_patient = {"name": "Frank", "id": 103, "blood_types": "O+"}
+r = requests.post("http://127.0.0.1:5000/new_patient", json=new_patient)
+print(r.status_code)
+print(r.text)
+
+print("Try to add patient with wrong data type")
+new_patient = {"name": "Frank", "id": "103", "blood_type": "O+"}
+r = requests.post("http://127.0.0.1:5000/new_patient", json=new_patient)
+print(r.status_code)
+print(r.text)
+
+print("Try to add patient with incorrect blood type")
+new_patient = {"name": "Frank", "id": "103", "blood_type": "X+"}
+r = requests.post("http://127.0.0.1:5000/new_patient", json=new_patient)
+print(r.status_code)
+print(r.text)
+
+print("Add test result")
+new_test = {"id": 100, "test_name": "HDL", "test_results": 80}
+r = requests.post("http://127.0.0.1:5000/add_test", json=new_test)
+print(r.status_code)
+print(r.text)
+
+print("Add second test result")
+new_test = {"id": 100, "test_name": "LDL", "test_results": 60}
+r = requests.post("http://127.0.0.1:5000/add_test", json=new_test)
+print(r.status_code)
+print(r.text)
+
+print("Try to add test result to id that doesn't exist")
+new_test = {"id": 200, "test_name": "LDL", "test_results": 60}
+r = requests.post("http://127.0.0.1:5000/add_test", json=new_test)
+print(r.status_code)
+print(r.text)
+
+print("Try to add test with bad key")
+new_test = {"id": 100, "testname": "LDL", "test_results": 11}
+r = requests.post("http://127.0.0.1:5000/add_test", json=new_test)
+print(r.status_code)
+print(r.text)
+
+print("Try to add test with bad data type")
+new_test = {"id": 100, "testname": "LDL", "test_results": "LDL=22"}
+r = requests.post("http://127.0.0.1:5000/add_test", json=new_test)
 print(r.status_code)
 print(r.text)
--- server.py
+++ server.py
@@ -1,8 +1,16 @@
 from flask import Flask, jsonify, request
+import logging
 
 app = Flask(__name__)
 
 db = list()
+
+
+def init_db():
+    logging.basicConfig(filename="health_db.log", filemode='w',
+                        level=logging.DEBUG)
+    add_patient_to_database("Ann Ables", 100, "A+")
+    add_patient_to_database("Bob Boyles", 101, "B-")
 
 
 def add_patient_to_database(name, id, blood_type):
@@ -11,6 +19,7 @@ def add_patient_to_database(name, id, blood_type):
                    "blood_type": blood_type,
                    "tests": list()}
     db.append(new_patient)
+    logging.info("Added patient {}".format(name))
     print(db)
 
 
@@ -25,21 +34,65 @@ def post_new_patient():
 
 
 def process_new_patient(in_data):
-    validate_input, server_status = validate_new_patient_data(in_data)
+    expected_key = ["name", "id", "blood_type"]
+    expected_types = [str, int, str]
+    validate_input = validate_post_input(in_data, expected_key, expected_types)
     if validate_input is not True:
-        return validate_input, server_status
+        return validate_input, 400
+    valid_blood_type = validate_blood_type(in_data)
+    if valid_blood_type is not True:
+        return valid_blood_type, 400
     add_patient_to_database(in_data["name"], in_data["id"],
                             in_data["blood_type"])
     return "Patient successfully added", 200
 
 
-def validate_new_patient_data(in_data):
-    expected_key = ["name", "id", "blood_type"]
-    for key in expected_key:
+def validate_post_input(in_data, expected_key, expected_types):
+    for key, v_type in zip(expected_key, expected_types):
         if key not in in_data.keys():
-            return "{} key not found in input".format(key), 400
-    return True, 200
+            return "{} key not found in input".format(key)
+        if type(in_data[key]) != v_type:
+            return "{} key value has the wrong variable type".format(key)
+    return True
+
+
+def validate_blood_type(in_data):
+    blood_types = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]
+    if in_data["blood_type"] not in blood_types:
+        return "{} is not a valid blood type".format(in_data["blood_type"])
+    return True
+
+
+@app.route("/add_test", methods=["POST"])
+def post_add_test():
+    in_data = request.get_json()
+    answer, status_code = process_add_test(in_data)
+    return answer, status_code
+
+
+def process_add_test(in_data):
+    expected_key = ["id", "test_name", "test_results"]
+    expected_types = [int, str, int]
+    valid_data = validate_post_input(in_data, expected_key, expected_types)
+    if valid_data is not True:
+        return valid_data, 400
+    patient = find_correct_patient(in_data["id"])
+    if patient is False:
+        return "Could not find patient in database", 400
+    data_to_add = (in_data["test_name"], in_data["test_results"])
+    patient["tests"].append(data_to_add)
+    logging.info("Added test to patient {}".format(patient["name"]))
+    print(db)
+    return "Test successfully added", 200
+
+
+def find_correct_patient(patient_id):
+    for patient in db:
+        if patient["id"] == patient_id:
+            return patient
+    return False
 
 
 if __name__ == '__main__':
+    init_db()
     app.run()
